f82e91d771c59ce38e4c3d5f47b743caf1763973
Sanju Dhritlahre <romeyoboy765@gmail.com>
2022-08-17 23:44:36 +0530

    Development Stage 04 Is Completed

--- lib/custom/custom_card.dart
+++ lib/custom/custom_card.dart
@@ -5,26 +5,46 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: const CircleAvatar(
-        radius: 30.0,
-      ),
-      title: const Text(
-        "Sanju Dhritlahre",
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-          fontSize: 17.0,
-        ),
-      ),
-      subtitle: Row(
-        children: const [
-          Icon(Icons.done_all_rounded, size: 20.0, color: Colors.lightBlue,),
-          SizedBox(width: 5.0,),
-          Text("Hi, How are you?")
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: <Widget> [
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              child: Image.asset("assets/images/group.png"),
+            ),
+            title: const Text(
+              "Sanju Dhritlahre",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0,
+              ),
+            ),
+            subtitle: Row(
+              children: const [
+                Icon(
+                  Icons.done_all_rounded,
+                  size: 20.0,
+                  color: Colors.lightBlue,
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text("Hi, How are you?")
+              ],
+            ),
+            trailing: const Text("18:40"),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 20.0, left: 80.0),
+            child: Divider(
+              thickness: 1.0,
+            ),
+          ),
         ],
       ),
-      trailing: const Text("18:40"),
     );
   }
 }
---
+++ lib/modal/chat_modal.dart
@@ -0,0 +1,15 @@
+class ChatModal {
+  String name;
+  String icon;
+  bool isGroup;
+  String time;
+  String currentMessage;
+  
+  ChatModal({
+    required this.name,
+    required this.icon,
+    required this.isGroup,
+    required this.time,
+    required this.currentMessage,
+  });
+}
