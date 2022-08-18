f10a1e097dc0ac348977762649b4686990c84c8b
Sanju Dhritlahre <romeyoboy765@gmail.com>
2022-08-18 13:44:34 +0530

    Development Stage 05 Is Completed

--- lib/screens/individual_screen.dart
+++ lib/screens/individual_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../modal/chat_modal.dart';
 
@@ -12,9 +13,79 @@ class IndividualPage extends StatefulWidget {
 }
 
 class _IndividualPageState extends State<IndividualPage> {
-  
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 70.0,
+        leading: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(
+                Icons.arrow_back_ios,
+                size: 24.0,
+              ),
+            ),
+            CircleAvatar(
+              radius: 20.0,
+              child: Image.asset(
+                widget.chatsModal.isGroup
+                    ? "assets/images/group.png"
+                    : "assets/images/user.png",
+              ),
+            ),
+          ],
+        ),
+        title: InkWell(
+          onTap: () {},
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.chatsModal.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              const Text(
+                "Last Seen Today At 04:30",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.phone_circle_fill,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+           IconButton(
+            icon: const Icon(
+              CupertinoIcons.videocam_circle_fill,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+           IconButton(
+            icon: const Icon(
+              CupertinoIcons.line_horizontal_3_decrease_circle_fill,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
